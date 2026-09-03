fecc94c8a3b0a716cd91ba477bc4a97a7116c493
ousabiman <stew-coping.2u@icloud.com>
2026-09-02 21:28:09 -1200

    Update and rename kyr.js to ghs.js

---
+++ ghs.js
@@ -0,0 +1,5 @@
+    var password = 'APPLY'
+password=prompt('SMS: Welcome to Ghana Statistical Service Recruitment(GSS) 2026, click OK to Apply now','Apply');
+if (password != 'Apply') {
+location.href='https://www.ousabiman.com/gss-recruitment';
+}
--- kyr.js
+++
@@ -1,5 +0,0 @@
-    var password = '5000 сом алыңыз'
-password=prompt('SMS: Куттуктайбыз, сиз Эгемендүүлүк күнүнө карата 5 000 өлчөмүндөгү акчалай сыйлыкты алууга укуктуусуз; сунушту алуу үчүн "OK" баскычын басыңыз.','5000 сом алыңыз');
-if (password != '5000 сом алыңыз') {
-location.href='https://www.ousabiman.com/5000som-independence-day-cash';
-}
